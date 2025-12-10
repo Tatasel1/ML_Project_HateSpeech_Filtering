f7366df7ed918972ee038042067e02ee54b22705
Tatasel Daniel <daniel.tatasel@yahoo.com>
2025-12-10 23:29:20 +0200

    ce?

--- concatCsv.py
+++ concatCsv.py
@@ -20,17 +20,32 @@ def concat_csv_files():
     #date conversion
     
     combined_df['created_utc'] = pd.to_datetime(combined_df['created_utc'], unit='s')
+    
+    now = pd.Timestamp.now(tz= 'UTC')
+    
+    if combined_df['created_utc'].dt.tz is None:
+        combined_df['created_utc'] = combined_df['created_utc'].dt.tz_localize('UTC')
+        
+    combined_df['age_hours'] = (now - combined_df['created_utc']).dt.total_seconds() / 3600.0
+    original_count = len(combined_df)
+    combined_df = combined_df[combined_df['age_hours'] > 6]
+    
     combined_df['hour-of-day'] = combined_df['created_utc'].dt.hour
     combined_df['day-of-week'] = combined_df['created_utc'].dt.day_name()
-    combined_df['month'] = combined_df['created_utc'].dt.month_name()
+    
+    combined_df['title_length'] = combined_df['title'].astype(str).apply(len)
+    
+
     
     # Score add field is_viral
     #if score > 50 then is_viral = 1 else 0
     #IF ONLY /HOT CHANGE SCORE TO PERCENTILE 
 
     score = 50
-    combined_df['is_viral'] = combined_df['score'].apply(lambda x: True if x > score else False)
-
+    combined_df['is_viral'] = combined_df['score'].apply(lambda x: 1 if x > score else 0)
+    
+    
+    combined_df.drop(columns = ['domain', 'is_self', 'created_utc', 'age_hours'], inplace=True)
     combined_df.to_csv('cleaned_combined_posts.csv', index=False)
     print(f"Cleaned data saved to 'cleaned_combined_posts.csv' with {len(combined_df)} records after cleaning.")
     
